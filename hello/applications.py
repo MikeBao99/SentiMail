237821dd231b479e71fad5b9561ec9326a346f0a
FacistFuhrer <mikebao1999@gmail.com>
2017-11-03 23:09:32 -0400

    Flask integration

--- hello/applications.py
+++ hello/applications.py
@@ -1,1 +1,32 @@
 
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# Configure session to use filesystem (instead of signed cookies)
+# app.config["SESSION_FILE_DIR"] = mkdtemp()
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
+
+@app.route("/")
+def index():
+    if request.method == "POST":
+    	render_template("index.html", result = "This FUCKING WORKED!!")
+    else:
+    	render_template("index.html", result = "we fucked up!!!")
