5c7cbafb8f7f621043d27b8cc08fa0705635c8ff
Mike Bao <mikebao@college.harvard.edu>
2017-11-05 01:37:39 -0400

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -4,19 +4,19 @@ from flask import Flask, flash, redirect, render_template, request, session
 
 from .models import Greeting
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-# # Create your views here.
-# def index(request):
-#     # return HttpResponse('Hello from Python!')
-#     return render(request, 'index.html')
-
-@app.route("/")
+# Create your views here.
 def index(request):
-    if request.method == "POST":
-    	render_template("index.html", result = "This FUCKING WORKED!!")
-    else:
-    	render_template("index.html", result = "we fucked up!!!")
+    # return HttpResponse('Hello from Python!')
+    return render(request, 'index.html')
+
+# @app.route("/")
+# def index(request):
+#     if request.method == "POST":
+#     	render_template("index.html", result = "This FUCKING WORKED!!")
+#     else:
+#     	render_template("index.html", result = "we fucked up!!!")
 
 def db(request):
 
